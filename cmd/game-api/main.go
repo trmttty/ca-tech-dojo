dd3c3969b1f17a5155ff727bd024b24ecde1d2ee
trmttty <tatsuya.tarumoto@gmail.com>
2020-09-28 21:55:35 +0900

    modified

--- cmd/game-api/main.go
+++ cmd/game-api/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/trmttty/ca-tech-dojo/api/server"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/trmttty/ca-tech-dojo/pkg/controllers"
+)
+
+func init() {
+	// get env
+}
+
+// Run say hello
 func main() {
-	server.Run()
+
+	http.HandleFunc("/", hello)
+	log.Fatalln(http.ListenAndServe(":8080", nil))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, controllers.Test)
 }
