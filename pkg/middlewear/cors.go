689bfd7e2eaecfd3b8dff4d584ae2e20a6e43d86
trmttty <tatsuya.tarumoto@gmail.com>
2020-10-02 11:49:47 +0900

    modified

--- pkg/middlewear/cors.go
+++ pkg/middlewear/cors.go
@@ -7,7 +7,7 @@ import (
 func Cors(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, x-token")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept, x-token")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Content-type", "application/json")
 		if r.Method == http.MethodOptions {
